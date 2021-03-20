7fc5337e9c468eadd8c50a820d22c1cfae8260aa
Marcio Junior <marciofmjr@hotmail.com>
2021-03-20 16:03:35 -0300

    teste

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
-  // var page = $("body").id
+  var page = $("body").attr('id');
+
+  if (page === "page_product") {
+    tagProductView();
+  }
 });
+
+function tagProductView() {
+  console.log("disparou product view");
+  var productName = $("#product_name").html();
+  gtag('event', 'view_item', {
+    items: [{
+      id: "P12345",
+      name: productName,
+      brand: "Google",
+      category: "Apparel/T-Shirts",
+      coupon: "SUMMER_DISCOUNT",
+      list_name: "Search Results",
+      list_position: 1,
+      price: 14.99,
+      quantity: 2,
+      variant: "Black"
+    }]
+  });
+}
